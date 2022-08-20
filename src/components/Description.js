e4080e2b37c097f329f260d918d88dfd40f2dce0
Veronica isla <veroislagleaniz@gmail.com>
2022-08-20 21:33:24 +0200

    added cart in header, and started with layout computer version of description section

--- src/components/Description.js
+++ src/components/Description.js
@@ -73,7 +73,7 @@ function Description(props) {
     <>
       <section className="description">
         {mobileDescription != null ? (
-          <article>
+          <article className="description__descAndImg">
             {/* IMG */}
             <div className="description__img">
               <Image mobile={props.mobile} />
@@ -132,6 +132,34 @@ function Description(props) {
                 </p>
                 <p>CPU: {mobileDescription.cpu}</p>
               </div>
+              <form action="" className="description__form">
+                <label htmlFor="">Almacenamiento: </label>
+                <select
+                  name="storageCode"
+                  id=""
+                  onChange={handleChangeMemory}
+                  value={internalMemory}
+                >
+                  <option value="" disabled>
+                    Seleccione
+                  </option>
+                  {renderMemory()}
+                </select>
+                <label htmlFor="" className="description__form__color">
+                  Color:{' '}
+                </label>
+                <select
+                  name="colorCode"
+                  id=""
+                  onChange={handleChangeColor}
+                  value={color}
+                >
+                  <option value="" disabled>
+                    Seleccione
+                  </option>
+                  {renderColor()}
+                </select>
+              </form>
             </div>
 
             {/* --> MOVIL SELECCIONADO */}
@@ -145,65 +173,34 @@ function Description(props) {
         ) : (
           'Cargando...'
         )}
-
-        {/*  SECCIÓN ACTIONS */}
-
-        {/* --> SELECTORES */}
-
-        <form action="">
-          <label htmlFor="">Almacenamiento: </label>
-          <select
-            name="storageCode"
-            id=""
-            onChange={handleChangeMemory}
-            value={internalMemory}
-          >
-            <option value="" disabled>
-              Seleccione
-            </option>
-            {renderMemory()}
-          </select>
-          <label htmlFor="">Color: </label>
-          <select
-            name="colorCode"
-            id=""
-            onChange={handleChangeColor}
-            value={color}
-          >
-            <option value="" disabled>
-              Seleccione
-            </option>
-            {renderColor()}
-          </select>
-        </form>
-
-        {/* --> ADD TO CART */}
-        <div className="description__btn">
-          <button type="button" className="description__btn__add">
-            añadir a la cesta
-          </button>
-
-          {/* BACK TO HOME */}
-
-          <Link to="/">
-            <button type="text" class="description__btn__home">
-              <svg
-                height="16"
-                width="16"
-                xmlns="http://www.w3.org/2000/svg"
-                version="1.1"
-                viewBox="0 0 1024 1024"
-              >
-                <path d="M874.690416 495.52477c0 11.2973-9.168824 20.466124-20.466124 20.466124l-604.773963 0 188.083679 188.083679c7.992021 7.992021 7.992021 20.947078 0 28.939099-4.001127 3.990894-9.240455 5.996574-14.46955 5.996574-5.239328 0-10.478655-1.995447-14.479783-5.996574l-223.00912-223.00912c-3.837398-3.837398-5.996574-9.046027-5.996574-14.46955 0-5.433756 2.159176-10.632151 5.996574-14.46955l223.019353-223.029586c7.992021-7.992021 20.957311-7.992021 28.949332 0 7.992021 8.002254 7.992021 20.957311 0 28.949332l-188.073446 188.073446 604.753497 0C865.521592 475.058646 874.690416 484.217237 874.690416 495.52477z"></path>
-              </svg>
-              <span class="description__btn__underline hover-underline-animation">
-                {' '}
-                Back to home{' '}
-              </span>
-            </button>
-          </Link>
-        </div>
       </section>
+
+      {/* --> ADD TO CART */}
+      <div className="description__btn">
+        <button type="button" className="description__btn__add">
+          añadir a la cesta
+        </button>
+
+        {/* BACK TO HOME */}
+
+        <Link to="/">
+          <button type="text" class="description__btn__home">
+            <svg
+              height="16"
+              width="16"
+              xmlns="http://www.w3.org/2000/svg"
+              version="1.1"
+              viewBox="0 0 1024 1024"
+            >
+              <path d="M874.690416 495.52477c0 11.2973-9.168824 20.466124-20.466124 20.466124l-604.773963 0 188.083679 188.083679c7.992021 7.992021 7.992021 20.947078 0 28.939099-4.001127 3.990894-9.240455 5.996574-14.46955 5.996574-5.239328 0-10.478655-1.995447-14.479783-5.996574l-223.00912-223.00912c-3.837398-3.837398-5.996574-9.046027-5.996574-14.46955 0-5.433756 2.159176-10.632151 5.996574-14.46955l223.019353-223.029586c7.992021-7.992021 20.957311-7.992021 28.949332 0 7.992021 8.002254 7.992021 20.957311 0 28.949332l-188.073446 188.073446 604.753497 0C865.521592 475.058646 874.690416 484.217237 874.690416 495.52477z"></path>
+            </svg>
+            <span class="description__btn__underline hover-underline-animation">
+              {' '}
+              Back to home{' '}
+            </span>
+          </button>
+        </Link>
+      </div>
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,22 +3,24 @@ import { Link } from 'react-router-dom';
 
 import '../styles/components/Header.scss';
 import logo from '../images/logo.white.PNG';
+import { BsBag } from 'react-icons/bs';
 
 function Header() {
   return (
     <header className="header">
-      <Link to={'/'}>
-        {' '}
-        <img
-          src={logo}
-          alt="Logo Napptilus"
-          title="Página principal"
-          className="header__logo"
-        />
-      </Link>
-      <div>
-        <h1 className="header__title">Napptilus</h1>
+      <div className="header__icons">
+        <Link to={'/'}>
+          <img
+            src={logo}
+            alt="Logo Napptilus"
+            title="Página principal"
+            className="header__logo"
+          />
+        </Link>
+        <BsBag className="header__cart" />
       </div>
+
+      <h1 className="header__title">Napptilus</h1>
     </header>
   );
 }
